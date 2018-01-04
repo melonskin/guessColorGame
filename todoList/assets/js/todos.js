2c8ce460a43cc805fb32a0c77b3c6f8037dbef94
melonskin <tsjtux@gmail.com>
2018-01-03 21:24:51 -0600

    logic part done

--- todoList/assets/js/todos.js
+++ todoList/assets/js/todos.js
@@ -1,1 +1,23 @@
-console.log("js connected");
+$("ul").on("click", ".todoContent", function() {
+    $(this).toggleClass("crossed");
+});
+
+$("ul").on("click", ".delete", function() {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+});
+
+var liPrefix = '<li><span class="delete">X</span><span class="todoContent">';
+var liEnd = '</span></li>';
+$("#newTodo").keypress(function(event) {
+    if (event.which == 13) {
+        var val = $(this).val();
+        if (!val) {
+            return;
+        }
+        // create new li
+        $("ul").append(liPrefix + val + liEnd);
+        $(this).val("");
+    }
+});
