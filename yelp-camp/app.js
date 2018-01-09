1454494dfe57966ad1f3719b84639e1e0efda7b7
Skin Tang <tsjtux@gmail.com>
2018-01-09 04:46:15 +0000

    add styles

--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -1,21 +1,43 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// tmp
+var campgrounds = [
+   {name: "name1", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg"},
+   {name: "name2", image: "http://www.travelbirbilling.com/wp-content/uploads/camp-pic1.jpg"},
+   {name: "name3", image: "http://www.camping.hr/cmsmedia/katalog/724/140-camp-turist-indian-tents.jpg"},
+   {name: "name1", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg"},
+   {name: "name2", image: "http://www.travelbirbilling.com/wp-content/uploads/camp-pic1.jpg"},
+   {name: "name3", image: "http://www.camping.hr/cmsmedia/katalog/724/140-camp-turist-indian-tents.jpg"},
+   {name: "name1", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg"},
+   {name: "name2", image: "http://www.travelbirbilling.com/wp-content/uploads/camp-pic1.jpg"},
+   {name: "name3", image: "http://www.camping.hr/cmsmedia/katalog/724/140-camp-turist-indian-tents.jpg"}
+];
+    
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
+
 app.get("/campgrounds", function(req, res) {
-   var campgrounds = [
-       {name: "name1", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg"},
-       {name: "name2", image: "http://www.travelbirbilling.com/wp-content/uploads/camp-pic1.jpg"},
-       {name: "name3", image: "http://www.camping.hr/cmsmedia/katalog/724/140-camp-turist-indian-tents.jpg"}
-    ];
-    
     res.render("campgrounds", {campgrounds: campgrounds});
 });
+
+app.post("/campgrounds", function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    campgrounds.push({name: name, image: image});
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs");    
+});
+
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("App started...");
 });
