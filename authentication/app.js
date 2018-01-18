7308fa299283e1747a4d58d1dc1932d8174389d1
Skin Tang <tsjtux@gmail.com>
2018-01-18 04:19:25 +0000

    add login

--- authentication/app.js
+++ authentication/app.js
@@ -18,11 +18,13 @@ app.use(require("express-session")({
 }));
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 // initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 // encode and put in session; decode and get from session
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -34,6 +36,40 @@ app.get("/", function(req, res) {
 app.get("/secret", function(req, res) {
     res.render("secret");
 })
+
+// auth routes
+// show sign up form
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+// create new user
+app.post("/register", function(req, res) {
+    // pass password as the second arg
+    User.register(new User({username: req.body.username}), 
+                req.body.password, function(err, user) {
+                    if (err) {
+                        console.log(err);
+                        res.render("register");
+                    } else {
+                        passport.authenticate("local")(req, res, function() {
+                            res.redirect("/secret");
+                        });
+                    }
+                })
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+//middleware, codes run before final call back. run immediately
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+    }), function(req, res) {
+    
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("App started...");
