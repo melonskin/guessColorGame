7d9f3115bbf783d00d1f1f617c8003228aab5511
Skin Tang <tsjtux@gmail.com>
2018-01-18 03:32:24 +0000

    auth add setup

--- authentication/app.js
+++ authentication/app.js
@@ -1,10 +1,31 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose"),
+    User                    = require("./models/user");
+    
+
+mongoose.connect("mongodb://localhost/auth_demo_app");
 var app = express();
 
-// mongoose.connect("mongodb://localhost/auth_demo_app");
+// use session
+app.use(require("express-session")({
+    secret: "my secret word",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.set("view engine", "ejs");
 
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// encode and put in session; decode and get from session
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
    res.render("home"); 
